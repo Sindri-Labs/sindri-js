dfa02edacda5fde69d3497a1ce12e1259b6e0952
Evan Sangaline <esangaline@gmail.com>
2023-11-17 18:37:31 -0700

    Roughly integrate commander.js.

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,3 +1,18 @@
 #! /usr/bin/env node
+import { Command } from "@commander-js/extra-typings";
+import process from "process";
 
-console.log("CLI");
+const loginCommand = new Command()
+  .name("login")
+  .description("Authorize the client.")
+  .action(() => {
+    console.log("hey");
+  });
+
+const program = new Command()
+  .name("sindri")
+  .description("The Sindri CLI client.")
+  .version("1.0.0") // TODO: Pull this in for real.
+  .addCommand(loginCommand);
+
+program.parse(process.argv);
