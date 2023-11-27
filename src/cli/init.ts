71d78fb4ab144d212abb0b84d944afccc785b733
Evan Sangaline <esangaline@gmail.com>
2023-11-27 14:00:18 -0600

    Add a rough cut of templating gnark without the actual circuit files.

--- src/cli/init.ts
+++ src/cli/init.ts
@@ -1,11 +1,12 @@
-import { existsSync, lstatSync, mkdirSync, readdirSync } from "fs";
+import { existsSync, statSync, mkdirSync, readdirSync } from "fs";
 import path from "path";
 import process from "process";
 
 import { Command } from "@commander-js/extra-typings";
-import { confirm } from "@inquirer/prompts";
+import { confirm, input, select } from "@inquirer/prompts";
 
 import { logger } from "cli/logging";
+import { scaffoldDirectory } from "cli/utils";
 
 export const initCommand = new Command()
   .name("init")
@@ -23,7 +24,7 @@ export const initCommand = new Command()
     // Ensure that the directory exists.
     if (!existsSync(directoryPath)) {
       mkdirSync(directoryPath, { recursive: true });
-    } else if (!lstatSync(directoryPath).isDirectory()) {
+    } else if (!statSync(directoryPath).isDirectory()) {
       logger.warn(
         `File "${directoryPath}" exists and is not a directory, aborting.`,
       );
@@ -45,5 +46,42 @@ export const initCommand = new Command()
       }
     }
 
-    // TODO: more
+    // Collect common fields.
+    const circuitName = await input({
+      message: "Circuit Name:",
+      default: directoryName,
+    });
+    const circuitType: "circom" | "gnark" | "halo2" | "noir" = await select({
+      message: "Select a Team:",
+      default: "gnark",
+      choices: [
+        { name: "Circom", value: "circom" },
+        { name: "Gnark", value: "gnark" },
+        { name: "Halo2", value: "halo2" },
+        { name: "Noir", value: "noir" },
+      ],
+    });
+    const context: object = { circuitName, circuitType };
+
+    // Handle individual circuit types.
+    if (circuitType === "gnark") {
+      const provingScheme: "groth16" = await select({
+        message: "Proving Scheme:",
+        default: "groth16",
+        choices: [{ name: "Groth16", value: "groth16" }],
+      });
+      const curveName: "bn254" = await select({
+        message: "Curve Name:",
+        default: "bn254",
+        choices: [{ name: "BN254", value: "bn254" }],
+      });
+      Object.assign(context, { curveName, provingScheme });
+    } else {
+      logger.fatal(`Sorry, ${circuitType} is not yet supported.`);
+      return process.exit(1);
+    }
+
+    // Perform the scaffolding.
+    await scaffoldDirectory("common", directoryPath, context);
+    await scaffoldDirectory(circuitType, directoryPath, context);
   });
