208ef446917f08cad0cf2b64e51663372d914926
Katie McKeon <katiemckeonphd@gmail.com>
2023-11-28 19:29:12 +0000

    without rm, consecutive package names dont compile

--- src/cli/init.ts
+++ src/cli/init.ts
@@ -37,13 +37,15 @@ export const initCommand = new Command()
     if (existingFiles.length > 0) {
       const proceed = await confirm({
         message:
-          `The "${directoryPath}" directory already exists and contains files. Continuing may ` +
+          `The "${directoryPath}" directory already exists and contains files. Continuing will ` +
           "overwrite your files. Are you *SURE* you would like to proceed?",
         default: false,
       });
       if (!proceed) {
         logger.info("Aborting.");
         return process.exit(1);
+      } else { 
+        rmSync(directoryPath, { recursive: true, force: true });
       }
     }
 
