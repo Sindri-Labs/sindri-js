a8d4d2f7d11fa0ba28493ed460613a96c648c312
Evan Sangaline <esangaline@gmail.com>
2023-11-29 16:40:14 -0600

    Add the start of a lint command.

---
+++ src/cli/lint.ts
@@ -0,0 +1,38 @@
+import { existsSync } from "fs";
+import path from "path";
+import process from "process";
+
+import { Command } from "@commander-js/extra-typings";
+
+import { logger } from "cli/logging";
+import { findFileUpwards } from "cli/utils";
+
+export const lintCommand = new Command()
+  .name("lint")
+  .description("Lint the current `sindri.json` project for potential issues.")
+  .argument(
+    "[directory]",
+    "The directory where the new project should be initialized.",
+    ".",
+  )
+  .action(async (directory) => {
+    // Find `sindri.json` and move into the root of the project directory.
+    const directoryPath = path.resolve(directory);
+    if (!existsSync(directoryPath)) {
+      logger.error(
+        `The "${directoryPath}" directory does not exist. Aborting.`,
+      );
+      return process.exit(1);
+    }
+    const sindriJsonPath = findFileUpwards(/^sindri.json$/i, directoryPath);
+    if (!sindriJsonPath) {
+      logger.error(
+        `No "sindri.json" file was found in or above "${directoryPath}". Aborting.`,
+      );
+      return process.exit(1);
+    }
+    logger.debug(`Found "sindri.json" at "${sindriJsonPath}".`);
+    const rootDirectory = path.dirname(sindriJsonPath);
+    logger.debug(`Changing current directory to "${rootDirectory}".`);
+    process.chdir(directoryPath);
+  });
