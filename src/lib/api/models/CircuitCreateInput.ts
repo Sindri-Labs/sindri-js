a4b5f19a711dc507586bdfe86ff2d86aafa6bd2e
Evan Sangaline <esangaline@gmail.com>
2023-12-18 18:41:55 -0600

    Add the circuit create input.

---
+++ src/lib/api/models/CircuitCreateInput.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * Client input when creating a circuit.
+ */
+export type CircuitCreateInput = {
+  tags: Array<string>;
+};
