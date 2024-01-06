7f4ce5ba8a26c41d4c0e4c9fb2deb4cfdfeb4ad7
Evan Sangaline <esangaline@gmail.com>
2024-01-06 00:09:26 -0600

    Fix the conditional timeout logic.

--- test/ava.config.base.mjs
+++ test/ava.config.base.mjs
@@ -8,10 +8,10 @@ export default {
   nodeArguments: ["--loader=tsx"],
   // Increase the timeout if we expect to make live API calls.
   timeout: ["dryrun", "record", "update", "wild"].includes(
-    process.NOCK_BACK_MODE ?? "lockdown",
+    process.env.NOCK_BACK_MODE ?? "lockdown",
   )
-    ? 60000
-    : 10000,
+    ? "60s"
+    : "10s",
   // Use child processes instead of threads because notch isn't thread safe.
   workerThreads: false,
 };
