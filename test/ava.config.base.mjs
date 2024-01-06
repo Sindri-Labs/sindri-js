0fedbd294b8b96ba8737f63785790f0adca2bfcd
Evan Sangaline <esangaline@gmail.com>
2024-01-05 23:47:17 -0600

    Disable worker threads in ava.

--- test/ava.config.base.mjs
+++ test/ava.config.base.mjs
@@ -4,4 +4,6 @@ export default {
   },
   files: ["test/**/*.test.ts"],
   nodeArguments: ["--loader=tsx"],
+  // Use child processes instead of threads because notch isn't thread safe.
+  workerThreads: false,
 };
