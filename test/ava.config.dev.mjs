ee60ed806417dbde4d12c8db1d17795cd3d25ee9
Evan Sangaline <esangaline@gmail.com>
2024-01-07 12:33:31 -0600

    Simplify test setup
    The test configuration was previously fairly complicated with different
    `ava.config.*` and `tsconfig.*` files for testing against the source directory
    and different build outputs. This collapses everything down to a single
    configuration which runs all of the tests and only supports running them
    against build outputs. This also allowed simplifying the npm scripts
    significantly because fewer configurations are necessary. I added a `yarn
    test:watch` command which automatically handles rebuilding and rerunning tests
    after changes in development, so the development ergonomics here should be
    reasonably good aside from needing to wait a couple of extra seconds for the
    build. We can add back more complexity if/when we need, but I think it's better
    to start out with a simpler configuration that requires less knowledge to use.
    
    Merges #43

--- test/ava.config.dev.mjs
+++
@@ -1,6 +0,0 @@
-import baseConfig from "./ava.config.base.mjs";
-
-export default {
-  ...baseConfig,
-  files: ["test/lib/**/*.test.ts"],
-};
--- test/sdk.test.ts
+++ test/sdk.test.ts
@@ -1,20 +1,20 @@
 import lib from "lib";
 
-import { test, useNock } from "./useNock";
+import { test, useNock } from "test/utils/useNock";
 
 useNock();
 
-test("should get Sindri manifest schema JSON", async (t) => {
+test("fetch Sindri manifest schema JSON", async (t) => {
   const schema = await lib.getSindriManifestSchema();
   t.true(schema?.title?.includes("Sindri"));
 });
 
-test("should get Sindri manifest schema JSON duplicate", async (t) => {
+test("fetch Sindri manifest schema JSON in parallel", async (t) => {
   const schema = await lib.getSindriManifestSchema();
   t.true(schema?.title?.includes("Sindri"));
 });
 
-test("library can be imported", (t) => {
+test("import library", (t) => {
   t.true(
     lib.environment && ["development", "production"].includes(lib.environment),
   );
