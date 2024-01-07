46882bcb5d798d3305655184480a1b4ac7fe9965
Evan Sangaline <esangaline@gmail.com>
2024-01-07 11:40:20 -0600

    Fix typo in a comment.

--- ava.config.mjs
+++ ava.config.mjs
@@ -2,7 +2,7 @@ import path from "path";
 import process from "process";
 import { fileURLToPath } from "url";
 
-// Use the node tsconfig import paths.
+// Use the test tsconfig import paths.
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 process.env.TSX_TSCONFIG_PATH = path.join(__dirname, "tsconfig.test.json");
