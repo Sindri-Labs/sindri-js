1e9a5ad264488606be5a7810b3acd42c05d48b7c
Evan Sangaline <esangaline@gmail.com>
2024-01-12 14:13:48 -0600

    Add the start of an API client.

---
+++ src/lib/client.ts
@@ -0,0 +1,91 @@
+import {
+  CircuitsService,
+  OpenAPI,
+  type CircomCircuitInfoResponse,
+  type Halo2CircuitInfoResponse,
+  type GnarkCircuitInfoResponse,
+  type NoirCircuitInfoResponse,
+} from "lib/api";
+import { loadConfig } from "lib/config";
+
+// Re-export types from the API.
+export type {
+  CircomCircuitInfoResponse,
+  Halo2CircuitInfoResponse,
+  GnarkCircuitInfoResponse,
+  NoirCircuitInfoResponse,
+};
+export type CircuitInfoResponse = (
+  | CircomCircuitInfoResponse
+  | Halo2CircuitInfoResponse
+  | GnarkCircuitInfoResponse
+  | NoirCircuitInfoResponse
+)[];
+
+/**
+ * The options for authenticating with the API.
+ */
+export interface AuthOptions {
+  /**
+   * The API key to use for authentication.
+   */
+  apiKey?: string;
+  /**
+   * The base URL for the API.
+   */
+  baseUrl?: string;
+}
+
+/**
+ * The client for the API.
+ */
+export class Client {
+  /**
+   * Create a new API client.
+   */
+  constructor(authOptions: AuthOptions = {}) {
+    this.authorize(authOptions);
+  }
+
+  /**
+   * The current value of the client's API key.
+   */
+  get apiKey(): string | null {
+    if (OpenAPI.TOKEN && typeof OpenAPI.TOKEN !== "string") {
+      return null;
+    }
+    return OpenAPI.TOKEN || null;
+  }
+
+  /**
+   * The current value of the client's base URL.
+   */
+  get baseUrl(): string {
+    return OpenAPI.BASE;
+  }
+
+  authorize(authOptions: AuthOptions): boolean {
+    if (process.env.BROWSER_BUILD) {
+      OpenAPI.BASE = authOptions.baseUrl || "https://sindri.app";
+      OpenAPI.TOKEN = authOptions.apiKey;
+    } else {
+      const config = loadConfig();
+      OpenAPI.BASE =
+        authOptions.baseUrl ||
+        OpenAPI.BASE ||
+        process.env.SINDRI_BASE_URL ||
+        config.auth?.baseUrl ||
+        "https://sindri.app";
+      OpenAPI.TOKEN =
+        authOptions.apiKey ||
+        OpenAPI.TOKEN ||
+        process.env.SINDRI_API_KEY ||
+        config.auth?.apiKey;
+    }
+    return !!(OpenAPI.BASE && OpenAPI.TOKEN);
+  }
+
+  async listCircuits(): Promise<CircuitInfoResponse> {
+    return await CircuitsService.circuitList();
+  }
+}
