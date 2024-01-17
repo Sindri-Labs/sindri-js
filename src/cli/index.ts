6e9fb7421558b0a78e8dcc98836da70b6d6cf2da
Evan Sangaline <esangaline@gmail.com>
2024-01-17 11:20:20 -0700

    Use the `sindri` client auth when possible.

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,4 +1,5 @@
 #! /usr/bin/env node
+import assert from "assert";
 import { argv, exit } from "process";
 
 import { Command } from "@commander-js/extra-typings";
@@ -11,7 +12,7 @@ import { loginCommand } from "cli/login";
 import { logoutCommand } from "cli/logout";
 import { whoamiCommand } from "cli/whoami";
 import { loadPackageJson } from "cli/utils";
-import { Config } from "lib/config";
+import sindri from "lib";
 import { logger } from "lib/logging";
 
 export const program = new Command()
@@ -50,8 +51,9 @@ export const program = new Command()
     }
     logger.debug(`Set log level to "${logger.level}".`);
 
-    // Force the loading of the config before subcommands.
-    new Config();
+    // Make sure the client is loaded and initialized before any subcommands run.
+    // Note that this also initializes the config.
+    assert(sindri);
   });
 
 if (require.main === module) {
