5edd81ee828db0209d065433f039f37bbf629ecd
Evan Sangaline <esangaline@gmail.com>
2024-02-21 10:22:28 -0700

    Bump the API version to 1.6.7.

--- src/lib/api/core/OpenAPI.ts
+++ src/lib/api/core/OpenAPI.ts
@@ -21,7 +21,7 @@ export type OpenAPIConfig = {
 
 export const OpenAPI: OpenAPIConfig = {
   BASE: "https://sindri.app",
-  VERSION: "1.6.6",
+  VERSION: "1.6.7",
   WITH_CREDENTIALS: false,
   CREDENTIALS: "include",
   TOKEN: undefined,
