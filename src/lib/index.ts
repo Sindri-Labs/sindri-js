dfdb72c379e0f02ab5d6e3f3a64b21e1aa013c8b
Evan Sangaline <esangaline@gmail.com>
2024-03-07 16:19:42 -0700

    Revert "Force the CJS build to export the library under `.default`."
    This reverts commit 019fd033174238fd4d4dd88fba75e10ffba79556.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,6 @@
 import { SindriClient } from "./client";
 
 export default new SindriClient();
-export const someProperty = "hello";
 
 export type * from "./client";
 export type { LogLevel } from "./logging";
