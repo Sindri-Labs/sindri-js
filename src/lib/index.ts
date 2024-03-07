019fd033174238fd4d4dd88fba75e10ffba79556
Evan Sangaline <esangaline@gmail.com>
2024-03-07 16:19:07 -0700

    Force the CJS build to export the library under `.default`.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 import { SindriClient } from "./client";
 
 export default new SindriClient();
+export const someProperty = "hello";
 
 export type * from "./client";
 export type { LogLevel } from "./logging";
