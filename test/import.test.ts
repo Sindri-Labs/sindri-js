29a3e962c074957f720325ea618e2d3adaeb160e
Evan Sangaline <esangaline@gmail.com>
2024-03-07 12:56:23 -0700

    Add import tests.

---
+++ test/import.test.ts
@@ -0,0 +1,36 @@
+import test from "ava";
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+test.before(() => {
+  global.window = {} as Window & typeof globalThis;
+});
+
+test("cli", (t) => {
+  const cli = require("../dist/cli/index.js");
+  t.truthy(cli?.program);
+});
+
+test("lib browser cjs", (t) => {
+  const lib = require("../dist/lib/browser/index.js");
+  t.truthy(lib);
+  t.false("false" in lib, "library should not be nested under `default`");
+});
+
+test("lib browser mjs", async (t) => {
+  const { default: lib } = await import("../dist/lib/browser/index.mjs");
+  t.truthy(lib);
+  t.false("default" in lib);
+});
+
+test("lib node cjs", (t) => {
+  const lib = require("../dist/lib/index.js");
+  t.truthy(lib);
+  t.false("false" in lib, "library should not be nested under `default`");
+});
+
+test("lib node mjs", async (t) => {
+  const { default: lib } = await import("../dist/lib/index.mjs");
+  t.truthy(lib);
+  t.false("default" in lib);
+});
