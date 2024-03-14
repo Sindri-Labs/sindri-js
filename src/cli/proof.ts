badb6d5d248f568b281f76796c59f4a6b40b233e
Evan Sangaline <esangaline@gmail.com>
2024-03-14 12:59:27 -0500

    Get a rough version of `proof create` working given recent library changes.

--- src/cli/proof.ts
+++ src/cli/proof.ts
@@ -4,10 +4,10 @@ import process from "process";
 
 import { Command } from "@commander-js/extra-typings";
 
-import { Config } from "cli/config";
-import { logger, print } from "cli/logging";
 import { findFileUpwards } from "cli/utils";
-import { ApiError, CircuitsService } from "lib/api";
+import sindri from "lib";
+import { ApiError } from "lib/api";
+import { print } from "lib/logging";
 
 const readStdin = async (): Promise<string> => {
   let inputData = "";
@@ -22,22 +22,21 @@ const proofCreateCommand = new Command()
   .description("Create a proof for the circuit.")
   .option(
     "-i, --input <input>",
-    "Input file for the proof (defaults to stdin).",
+    "Input file for the proof (defaults to stdin in on-TTY; " +
+      "`input.json`, `example-input.json`, or `Prover.toml` otherwise).",
   )
   .option("-t, --tag <tag>", "Tag to generate the proof from.", "latest")
   .action(async ({ input, tag }) => {
-    // Authorize the API client.
-    const config = new Config();
-    const auth = config.auth;
-    if (!auth) {
-      logger.warn("You must login first with `sindri login`.");
+    // Check that the API client is authorized.
+    if (!sindri.apiKey || !sindri.baseUrl) {
+      sindri.logger.warn("You must login first with `sindri login`.");
       return process.exit(1);
     }
 
     // Find `sindri.json` and move into the root of the project directory.
     const currentDirectoryPath = path.resolve(".");
     if (!fs.existsSync(currentDirectoryPath)) {
-      logger.error(
+      sindri.logger.error(
         `The "${currentDirectoryPath}" directory does not exist. Aborting.`,
       );
       return process.exit(1);
@@ -47,14 +46,14 @@ const proofCreateCommand = new Command()
       currentDirectoryPath,
     );
     if (!sindriJsonPath) {
-      logger.error(
+      sindri.logger.error(
         `No "sindri.json" file was found in or above "${currentDirectoryPath}". Aborting.`,
       );
       return process.exit(1);
     }
-    logger.debug(`Found "sindri.json" at "${sindriJsonPath}".`);
+    sindri.logger.debug(`Found "sindri.json" at "${sindriJsonPath}".`);
     const rootDirectory = path.dirname(sindriJsonPath);
-    logger.debug(`Changing current directory to "${rootDirectory}".`);
+    sindri.logger.debug(`Changing current directory to "${rootDirectory}".`);
     process.chdir(rootDirectory);
 
     // Load `sindri.json` and find the circuit name.
@@ -64,19 +63,19 @@ const proofCreateCommand = new Command()
         encoding: "utf-8",
       });
       sindriJson = JSON.parse(sindriJsonContent);
-      logger.debug(
+      sindri.logger.debug(
         `Successfully loaded "sindri.json" from "${sindriJsonPath}":`,
       );
-      logger.debug(sindriJson);
+      sindri.logger.debug(sindriJson);
     } catch (error) {
-      logger.fatal(
+      sindri.logger.fatal(
         `Error loading "${sindriJsonPath}", perhaps it is not valid JSON?`,
       );
-      logger.error(error);
+      sindri.logger.error(error);
       return process.exit(1);
     }
     if (!("name" in sindriJson)) {
-      logger.error('No "name" field found in "sindri.json". Aborting.');
+      sindri.logger.error('No "name" field found in "sindri.json". Aborting.');
       return process.exit(1);
     }
     const circuitName = sindriJson.name;
@@ -86,7 +85,7 @@ const proofCreateCommand = new Command()
     if (input && fs.existsSync(input)) {
       // Read from the specified input file.
       proofInput = fs.readFileSync(input, "utf-8");
-    } else if (!process.stdin.isTTY) {
+    } else if (!process.stdin.isTTY || input === "-") {
       // Read from stdin in a non-TTY context.
       proofInput = await readStdin();
     } else {
@@ -114,26 +113,28 @@ const proofCreateCommand = new Command()
 
     const circuitIdentifier = `${circuitName}:${tag}`;
     try {
-      const response = await CircuitsService.proofCreate(circuitIdentifier, {
-        proof_input: proofInput,
-      });
-      logger.debug(`/api/v1/circuit/${circuitIdentifier}/prove/ response:`);
-      logger.debug(response);
-      print({
-        proofId: response.proof_id,
-        proof: response.proof,
-        public: response.public,
-        verificationKey: response.verification_key,
-      });
+      const response = await sindri.proveCircuit(circuitIdentifier, proofInput);
+      print(
+        JSON.stringify(
+          {
+            proofId: response.proof_id,
+            proof: response.proof,
+            public: response.public,
+            verificationKey: response.verification_key,
+          },
+          null,
+          2,
+        ),
+      );
     } catch (error) {
       // TODO: Better error handling.
       if (error instanceof ApiError && error.status === 404) {
-        logger.error(
+        sindri.logger.error(
           `No circuit found with the name "${circuitName}" and tag "${tag}".`,
         );
       } else {
-        logger.fatal("An unknown error occurred.");
-        logger.error(error);
+        sindri.logger.fatal("An unknown error occurred.");
+        sindri.logger.error(error);
         return process.exit(1);
       }
     }
