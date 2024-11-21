8f4842440b2a971339665e3b8d6cf34d382a86c3
GitHub Action <action@github.com>
2024-11-21 15:00:41 +0000

    Automated update by GitHub Action.

--- src/lib/api/core/OpenAPI.ts
+++ src/lib/api/core/OpenAPI.ts
@@ -27,7 +27,7 @@ export type OpenAPIConfig = {
 
 export const OpenAPI: OpenAPIConfig = {
   BASE: "https://sindri.app",
-  VERSION: "1.14.3",
+  VERSION: "1.14.4",
   WITH_CREDENTIALS: false,
   CREDENTIALS: "include",
   TOKEN: undefined,
