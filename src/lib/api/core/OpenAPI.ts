889c4014f99506a56f9cdd67588f868692c9941f
Katie McKeon <katiemckeonphd@gmail.com>
2025-04-09 15:29:04 +0000

    rm rc1 suffix

--- src/lib/api/core/OpenAPI.ts
+++ src/lib/api/core/OpenAPI.ts
@@ -27,7 +27,7 @@ export type OpenAPIConfig = {
 
 export const OpenAPI: OpenAPIConfig = {
   BASE: "https://sindri.app",
-  VERSION: "1.17.13-rc1",
+  VERSION: "1.17.13",
   WITH_CREDENTIALS: false,
   CREDENTIALS: "include",
   TOKEN: undefined,
