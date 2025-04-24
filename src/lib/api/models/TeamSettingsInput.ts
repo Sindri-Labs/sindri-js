4a96ae0e0c1ce02d18e049f5a72826c0893568e9
GitHub Action <action@github.com>
2025-04-24 01:23:42 +0000

    Automated update by GitHub Action.

---
+++ src/lib/api/models/TeamSettingsInput.ts
@@ -0,0 +1,26 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * Client input to update team settings.
+ */
+export type TeamSettingsInput = {
+  /**
+   * The display name of the team.
+   */
+  display_name?: string | null;
+  /**
+   * The URL for the team's github account. This is displayed on the team page.
+   */
+  github_url?: string | null;
+  /**
+   * The URL for the team's Twitter account. This is displayed on the team page.
+   */
+  twitter_url?: string | null;
+  /**
+   * The URL for the team's website. This is displayed on the team page.
+   */
+  url?: string | null;
+};
